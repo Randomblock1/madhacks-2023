787dfb8ab6068189fcd22d9faca2a72fcd1ac85f
Benjamin G <randomblock1@pm.me>
2023-03-04 13:24:01 -0600

    use tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Altruist</h1>
+<h2>Volunteering for a better tomorrow</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
