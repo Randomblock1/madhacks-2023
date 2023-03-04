617551efda42e73ce40556570e898071bcea58e5
Benjamin G <randomblock1@pm.me>
2023-03-04 16:24:08 -0600

    more styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,4 @@
-<div class="container">
-	<header
-		class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
-	>
-		<span class="fs-1" style="font-family: 'Chonburi'">Altruist</span>
-
-		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-			<li><a href="/volunteers" class="nav-link px-2 link-dark">For Volunteers</a></li>
-			<li><a href="/organizations" class="nav-link px-2 link-dark">For Organizations</a></li>
-			<li><a href="/about" class="nav-link px-2 link-dark">About</a></li>
-		</ul>
-
-		<span class="text">
-			<button type="button" class="btn btn-outline-success text-nowrap me-2 rounded-pill">Login</button>
-			<button type="button" class="btn btn-success text-nowrap rounded-pill">Sign Up</button>
-        </span>
-		<svg width="100%" height="20" viewBox="0 0 667 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0.5 0L651.5 4.5L666.5 8.5H348.5H10.5L0.5 0Z" fill="black" />
-		</svg>
-	</header>
-</div>
 
 <div class="container">
-    <h1 style="font-family: 'Chonburi'">Volunteering for a better tomorrow</h1>
+    <h1 class="font-styled">Volunteering for a better tomorrow</h1>
 </div>
