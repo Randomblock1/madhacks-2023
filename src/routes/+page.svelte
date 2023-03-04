5b9a21772f33518d3fdb7bad675f547e63ff8cf7
Benjamin G <randomblock1@pm.me>
2023-03-04 16:11:03 -0600

    push main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Altruist</h1>
-<h2>Volunteering for a better tomorrow</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<header
+		class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
+	>
+		<span class="fs-1" style="font-family: 'Chonburi'">Altruist</span>
+
+		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+			<li><a href="/volunteers" class="nav-link px-2 link-dark">For Volunteers</a></li>
+			<li><a href="/organizations" class="nav-link px-2 link-dark">For Organizations</a></li>
+			<li><a href="/about" class="nav-link px-2 link-dark">About</a></li>
+		</ul>
+
+		<span class="text">
+			<button type="button" class="btn btn-outline-success text-nowrap me-2 rounded-pill">Login</button>
+			<button type="button" class="btn btn-success text-nowrap rounded-pill">Sign Up</button>
+        </span>
+		<svg width="100%" height="20" viewBox="0 0 667 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M0.5 0L651.5 4.5L666.5 8.5H348.5H10.5L0.5 0Z" fill="black" />
+		</svg>
+	</header>
+</div>
+
+<div class="container">
+    <h1 style="font-family: 'Chonburi'">Volunteering for a better tomorrow</h1>
+</div>
