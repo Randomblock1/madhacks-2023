2d74d5d2deb3dfa21d1b1b0647cb0779cf714475
Benjamin G <randomblock1@pm.me>
2023-03-04 20:01:01 -0600

    fix login and icon alignment

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,27 +2,39 @@
 	<span class="rounded-box">
 		<h1 class="font-styled">Login</h1>
 		<form action="process_login" method="post">
-			<label class="font-styled" style="padding-right: 37px" for="email"> Email: </label>
-			<input
-				class="rounded-2"
-				type="email"
-				id="email"
-				name="email"
-				placeholder="Input your email here"
-			/>
-			<div class="form-group pt-2">
-				<label class="font-styled" for="password">Password: </label>
-				<input
-					class="rounded-2"
-					type="password"
-					id="password"
-					name="password"
-					placeholder="Input your password here"
-				/>
+			<div class="row align-items-start p-2">
+				<div class="col">
+					<label class="font-styled" style="padding-right: 37px" for="email"> Email: </label>
+				</div>
+
+				<div class="col">
+					<input
+						class="rounded-2"
+						type="email"
+						id="email"
+						name="email"
+						placeholder="email@example.com"
+					/>
+				</div>
 			</div>
-			<div class="form-group pt-3">
-				<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
+
+			<div class="row p-2">
+				<div class="col">
+					<label class="font-styled" for="password">Password: </label>
+				</div>
+
+				<div class="col">
+					<input
+						class="rounded-2"
+						type="password"
+						id="password"
+						name="password"
+						placeholder="**********"
+					/>
+				</div>
 			</div>
+
+			<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
 		</form>
 	</span>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,27 +2,70 @@
 	<span class="rounded-box">
 		<h1 class="font-styled">Sign Up</h1>
 		<form action="/api/signup" method="post">
-			<p>
-				Account Type:
-				<input type="radio" name="type" value="volunteer" id="volunteer" checked /> Volunteer
-				<input type="radio" name="type" value="organization" id="organization" /> Organization
-			</p>
+			<div class="row align-items-start p-2">
+				<div class="col">
+					<p>Account Type:</p>
+				</div>
 
-			<label for="name">Enter your name: </label>
-			<input type="text" id="name" name="name" /><br /><br />
+				<div class="col">
+					<input type="radio" name="type" value="volunteer" id="volunteer" checked /> Volunteer
+				</div>
 
-			<label for="email">Enter your email: </label>
-			<input type="email" id="email" name="email" /><br /><br />
+				<div class="col">
+					<input type="radio" name="type" value="organization" id="organization" /> Organization
+				</div>
+			</div>
 
-			<label for="address">Enter your address: </label>
-			<input type="text" id="address" name="address" /><br /><br />
+			<div class="row p-2">
+				<div class="col">
+					<label for="name">Enter your name: </label>
+				</div>
 
-			<label for="password">Enter a password: </label>
-			<input type="password" id="password" name="password" /><br /><br />
+				<div class="col">
+					<input type="text" id="name" name="name" placeholder="John Doe" />
+				</div>
+			</div>
 
-			<label for="phone">Enter your phone number: </label>
-			<p><small>(Optional)</small></p>
-			<input type="tel" id="phone" name="phone" /><br /><br />
+			<div class="row p-2">
+				<div class="col">
+					<label for="email">Enter your email: </label>
+				</div>
+
+				<div class="col">
+					<input type="email" id="email" name="email" placeholder="email@example.com" />
+				</div>
+			</div>
+
+			<div class="row p-2">
+				<div class="col">
+					<label for="address">Enter your address: </label>
+				</div>
+
+				<div class="col">
+					<input type="text" id="address" name="address" placeholder="123 Real Steet, City, OH" />
+				</div>
+			</div>
+
+			<div class="row p-2">
+				<div class="col">
+					<label for="password">Enter a password: </label>
+				</div>
+
+				<div class="col">
+					<input type="password" id="password" name="password" placeholder="**********" />
+				</div>
+			</div>
+
+			<div class="row p-2">
+				<div class="col">
+					<label for="phone">Enter your phone number: </label>
+					<p><small><i>(Optional)</i></small></p>
+				</div>
+
+				<div class="col">
+					<input type="tel" id="phone" name="phone" placeholder="123-456-7890" />
+				</div>
+			</div>
 
 			<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
 		</form>
