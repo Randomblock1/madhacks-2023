9e371cfd579db4ad6582ac55a2b95ac502ad5d72
Balabalu-Ve <60904972+Balabalu-VE@users.noreply.github.com>
2023-03-04 21:17:59 -0600

    Main page looks nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,35 @@
 <div class="container">
 	<h1 class="font-styled">Volunteering for a better tomorrow</h1>
-	<p class="fs-4">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+	<table cellspacing="30">
+		<tr>
+			<td style="width:45%">
+				<a href="/volunteer">
+					<button class="btn btn-success rounded-pill font-styled space">I'm a volunteer</button></a
+				>
+				<br />
+				<p class="fs-4">
+					Here we connect volunteers with those seeking them. If you are looking to volunteer and
+					don't know where to start. This is the place!
+				</p>
+				<br />
 
-	<a href="/volunteer">
-		<button class="btn btn-success rounded-pill font-styled m-1">I'm a volunteer</button></a
-	>
-	<br />
-	<a href="/organization">
-		<button class="btn btn-success rounded-pill font-styled m-1">I'm looking for volunteers</button>
-	</a>
+				<a href="/organization">
+					<button class="btn btn-success rounded-pill font-styled m-1"
+						>I'm looking for volunteers</button
+					>
+				</a>
+				<p class="fs-4">
+					If you are an organization in desperate need of volunteers, we are here to help. List any
+					event that needs volunteers to fullfill your volunteering needs.
+				</p>
+			</td>
+			<td style="Width:80px">
+                
+				<br />
+			</td>
+			<td>
+				<img class="bi me-2 footer" src="/volunteer_icon.jpg" alt="I like men" />
+			</td>
+		</tr>
+	</table>
 </div>
