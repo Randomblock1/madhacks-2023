1c0150a277d8ad14e1b5f0f9d3d2103f95a45452
Balabalu-Ve <60904972+Balabalu-VE@users.noreply.github.com>
2023-03-05 01:18:20 -0600

    Some things

---
+++ src/routes/postMaker/+page.svelte
@@ -0,0 +1,75 @@
+<div class="container">
+    <div class="container h-100 d-flex align-items-center justify-content-center">
+        <span class="rounded-box">
+            <h1 class="font-styled">Create Volunteer Event</h1>
+            <form action="/api/signup" method="post">
+    
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="name">Enter name of event: </label>
+                    </div>
+    
+                    <div class="col">
+                        <input class = "sendRight" type="text" id="name" name="name" placeholder="Event name" />
+                    </div>
+                </div>
+    
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="time">Enter event start time: </label>
+                    </div>
+    
+                    <div class="col">
+                        <input class = "sendRight" type="time" id="timeStart" name="timeStart" placeholder=""/>
+                    </div>
+                </div>
+
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="timeEnd">Enter event end time: </label>
+                    </div>
+    
+                    <div class="col">
+                        <input class = "sendRight" type="time" id="timeEnd" name="timeEnd" placeholder=""/>
+                    </div>
+                </div>
+    
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="address">Enter address of event: </label>
+                    </div>
+    
+                    <div class="col">
+                        <input class = "sendRight" type="text" id="address" name="address" placeholder="123 Real Steet, City, OH" />
+                    </div>
+                </div>
+    
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="volunteerNum">Enter volunteers needed: </label>
+                    </div>
+    
+                    <div class="col">
+                        <input class = "smallText sendRight" type="number" id="volunteerNum" name="volunteerNum" placeholder="" />
+                    </div>
+                </div>
+    
+                <div class="row p-2">
+                    <div class="col">
+                        <label for="descriptions">Enter description ef event: </label>
+                        <p><small><i>(Optional)</i></small></p>
+                    </div>
+    
+                    <div class="col">
+                        <textarea  class = "largeText sendRight" id="descriptions" name="descriptions" placeholder="" />
+                    </div>
+                </div>
+    
+                <input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
+            </form>
+        </span>
+    </div>
+    
+
+    
+</div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -67,7 +67,30 @@
 				</div>
 			</div>
 
-			<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
+			<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit"  />
+			<!--on:click={handleClick}
+			<div>{status}</div>-->
 		</form>
 	</span>
 </div>
+
+<script> /*
+let status = '';
+
+const handleClick = async () => {
+	const sgMail = require('@sendgrid/mail')
+	//sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+	const msg = {
+  	to: 'test@example.com', // Change to your recipient
+  	from: 'test@example.com', // Change to your verified sender
+  	subject: 'Sending with SendGrid is Fun',
+	text: 'and easy to do anywhere, even with Node.js',
+  	html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+	}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+}*/
+</script>
