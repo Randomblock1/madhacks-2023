1346144145b3f603f79146df81fb99e3b000f281
Balabalu-Ve <60904972+Balabalu-VE@users.noreply.github.com>
2023-03-04 18:38:36 -0600

    centered

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,29 @@
 <div class="container">
 	<form action="process_login" method="post">
-		<div class="col-lg-4 col-lg-offset-4">
-			<label class="font-styled" for="email"> Email: </label>
-			<input
-				class="rounded-2 float-left"
-				type="text"
-				id="email"
-				name="email"
-				placeholder="Input your email here"
-			/><br /><br />
+		<div class="position-absolute top-50 start-50 translate-middle">
+			<span class="rounded-box">
+				<label class="font-styled" style="padding-right: 37px" for="email"> Email: </label>
+				<input
+					class="rounded-2 float-left"
+					type="text"
+					id="email"
+					name="email"
+					placeholder="Input your email here"
+				/>
+				<div class="form-group pt-2">
+					<label class="font-styled" for="password">Password: </label>
+					<input
+						class="rounded-2"
+						type="text"
+						id="password"
+						name="password"
+						placeholder="Input your password here"
+					/>
+				</div>
+				<div class="form-group pt-3">
+					<input class="font-styled rounded-pill float-right" type="submit" value="Submit" />
+				</div>
+			</span>
 		</div>
-		<div class="form-group">
-			<label class="font-styled" for="password">Password: </label>
-			<input
-				class="rounded-2"
-				type="text"
-				id="password"
-				name="password"
-				placeholder="Input Your Password Here"
-			/> <br /><br />
-		</div>
-		<input class="font-styled rounded-pill float-right" type="submit" value="Submit" />
 	</form>
 </div>
