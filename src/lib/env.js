2fa56779e1b3b9a867002f778bd1730ab3b7408d
Benjamin G <randomblock1@pm.me>
2023-03-05 08:40:19 -0600

    the api doesnt work over https for some reason

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,3 +1,3 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const API_URL = isProduction ? 'https://api.altruistapp.tech:8000' : 'http://localhost:8000';
+export const API_URL = isProduction ? 'http://api.altruistapp.tech:8000' : 'http://localhost:8000';
