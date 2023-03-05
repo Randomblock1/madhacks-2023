ba967cb01234da58972ef11d8e692085c4c3bb22
Benjamin G <randomblock1@pm.me>
2023-03-04 19:13:31 -0600

    fix signup/login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,29 +1,28 @@
-<div class="container">
-	<form action="process_login" method="post">
-		<div class="position-absolute top-50 start-50 translate-middle">
-			<span class="rounded-box">
-				<label class="font-styled" style="padding-right: 37px" for="email"> Email: </label>
+<div class="container h-100 d-flex align-items-center justify-content-center">
+	<span class="rounded-box">
+		<h1 class="font-styled">Login</h1>
+		<form action="process_login" method="post">
+			<label class="font-styled" style="padding-right: 37px" for="email"> Email: </label>
+			<input
+				class="rounded-2"
+				type="email"
+				id="email"
+				name="email"
+				placeholder="Input your email here"
+			/>
+			<div class="form-group pt-2">
+				<label class="font-styled" for="password">Password: </label>
 				<input
-					class="rounded-2 float-left"
-					type="text"
-					id="email"
-					name="email"
-					placeholder="Input your email here"
+					class="rounded-2"
+					type="password"
+					id="password"
+					name="password"
+					placeholder="Input your password here"
 				/>
-				<div class="form-group pt-2">
-					<label class="font-styled" for="password">Password: </label>
-					<input
-						class="rounded-2"
-						type="text"
-						id="password"
-						name="password"
-						placeholder="Input your password here"
-					/>
-				</div>
-				<div class="form-group pt-3">
-					<input class="font-styled rounded-pill float-right" type="submit" value="Submit" />
-				</div>
-			</span>
-		</div>
-	</form>
+			</div>
+			<div class="form-group pt-3">
+				<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
+			</div>
+		</form>
+	</span>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,82 +1,30 @@
-<script>
-	import { onMount } from 'svelte';
-	onMount(() => {
-		const option1 = document.getElementById('option1');
-		const option2 = document.getElementById('option2');
+<div class="container h-100 d-flex align-items-center justify-content-center">
+	<span class="rounded-box">
+		<h1 class="font-styled">Sign Up</h1>
+		<form action="/api/signup" method="post">
+			<p>
+				Account Type:
+				<input type="radio" name="type" value="volunteer" id="volunteer" checked /> Volunteer
+				<input type="radio" name="type" value="organization" id="organization" /> Organization
+			</p>
 
-		const display1 = document.getElementById('display1');
-		const display2 = document.getElementById('display2');
-
-		if (option1 && display1 && display2) {
-			display1.style.display = 'block';
-			option1.addEventListener('change', function () {
-				// Check if the radio button is checked
-				console.log('orireo');
-				display1.style.display = 'block';
-				display2.style.display = 'none';
-			});
-		}
-
-		if (option2 && display1 && display2) {
-			option2.addEventListener('change', function () {
-				// Check if the radio button is checked
-				console.log('ofsreo');
-				// The radio button is checked
-				display1.style.display = 'none';
-				display2.style.display = 'block';
-			});
-		}
-	});
-</script>
-
-<div class="container">
-	<input type="radio" name="option" value="option1" id="option1" checked /> Volunteer
-
-	<input type="radio" name="option" value="option2" id="option2" /> Organization seeking Volunteer
-
-	<div id="display1" style="display:none;">
-		Element 1
-		<form action="process_signup_volunteer" method = "post">
-			<label for="name">Enter name: </label>
+			<label for="name">Enter your name: </label>
 			<input type="text" id="name" name="name" /><br /><br />
 
-			<label for="email">Enter email: </label>
-			<input type="text" id="email" name="email" /><br /><br />
+			<label for="email">Enter your email: </label>
+			<input type="email" id="email" name="email" /><br /><br />
 
-			<label for="address">Enter Adress: </label>
+			<label for="address">Enter your address: </label>
 			<input type="text" id="address" name="address" /><br /><br />
 
-			<label for="password">Create password: </label>
-			<input type="text" id="password" name="password" /><br /><br />
+			<label for="password">Enter a password: </label>
+			<input type="password" id="password" name="password" /><br /><br />
 
-			<label for="password">Enter phone number: </label>
+			<label for="phone">Enter your phone number: </label>
 			<p><small>(Optional)</small></p>
-			<input type="text" id="password" name="password" /><br /><br />
+			<input type="tel" id="phone" name="phone" /><br /><br />
 
-			<input type="submit" value="Submit" />
+			<input class="btn btn-success font-styled rounded-pill" type="submit" value="Submit" />
 		</form>
-	</div>
-
-	<div id="display2" style="display:none;">
-		<form action="process_signup_volunteer" method = "post">
-			<label for="name">Enter organization: </label>
-			<input type="text" id="name" name="name" /><br /><br />
-
-			<label for="email">Enter email: </label>
-			<input type="text" id="email" name="email" /><br /><br />
-
-			<label for="address">Enter website: </label>
-			<p><small>(Optional)</small></p>
-			<input type="text" id="address" name="address" /><br /><br />
-
-			<label for="password">Create password: </label>
-			<input type="text" id="password" name="password" /><br /><br />
-
-			<label for="password">Enter phone number: </label>
-			<p><small>(Optional)</small></p>
-			<input type="text" id="password" name="password" /><br /><br />
-            
-			<input type="submit" value="Submit" />
-		</form>
-	</div>
+	</span>
 </div>
